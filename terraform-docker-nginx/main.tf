50ed30e57b51e1cac67a30fbe9428f49897b84aa
noahedwardhall <noahehall@users.noreply.github.com>
2021-10-24 06:59:50 -0400

    terraform: consume variables.tf in main.tf"

--- terraform-docker-nginx/main.tf
+++ terraform-docker-nginx/main.tf
@@ -18,7 +18,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = "tutorial"
+  name  = var.container_name
   ports {
     internal = 80
     external = 8081
