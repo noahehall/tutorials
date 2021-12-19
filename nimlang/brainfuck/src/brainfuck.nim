bdd747976535c777edb5c37a68613634af2c46bc
noahehall <10003239-nhall1@users.noreply.gitlab.com>
2021-12-19 03:02:38 -0500

    brainfuck

--- nimlang/brainfuck/src/brainfuck.nim
+++ nimlang/brainfuck/src/brainfuck.nim
@@ -1,5 +1,12 @@
 # This is just an example to get you started. A typical binary package
 # uses this file as the main entry point of the application.
 
+import os # read cmd line args
+
 when isMainModule:
-  echo("Hello, World!")
+  echo("Welcome to brainfuck!")
+
+  let code = if paramCount() == 1: readFile paramStr(1)
+    else: readAll stdin
+
+  echo code
